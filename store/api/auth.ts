d4ae1f48924f05d07bb229b96f964c33eb679e83
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-06-21 06:24:59 -0400

    refactor: remove unnecessary blank line in auth API file

--- store/api/auth.ts
+++ store/api/auth.ts
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
 import { SignUpInput, LogInInput, Token } from "../../utils/types";
 
 export const authApi = createApi({
